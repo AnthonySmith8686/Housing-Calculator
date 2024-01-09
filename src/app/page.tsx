0cb326b529bcc560bc4f08241a944acecca4bd48
AnthonySmith8686 <anthonysmith8686@gmail.com>
2024-01-09 20:20:25 +0000

    added iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Fragment } from "react";
 
 
 export default function Home() {
   return (
+    <Fragment>
     <main className="flex flex-col items-center justify-between p-24 bg-red-500 w-96 h-96">
       Hello!
     </main>
+    <iframe src="https://housing-calculator.vercel.app/" width="300" height="400" title="Calculator App"></iframe>
+    </Fragment>
   )
 }
